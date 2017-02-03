8fcfd3d7c1594d4ca224c3da325f2c52a3ca5f38
CRoot <UnixCRoot@outlook.com>
2017-02-03 20:08:52 +0800

    修复一些默默其妙的日志输出bug,会导致异常的403

--- waf.lua
+++ waf.lua
@@ -56,7 +56,7 @@ function _M.white_ip_check()
         if IP_WHITE_RULE ~= nil then
             for _, rule in pairs(IP_WHITE_RULE) do
                 if rule ~= "" and rulematch(WHITE_IP, rule, "jo") then
-                    _M.log_record('White_IP', ngx.var_request_uri, "_", "_")
+                    util.log_record(config.config_log_dir,'White_IP', ngx.var_request_uri, "_", "_")
                     return true
                 end
             end
@@ -86,7 +86,7 @@ function _M.black_ip_check()
         if IP_BLACK_RULE ~= nil then
             for _, rule in pairs(IP_BLACK_RULE) do
                 if rule ~= "" and rulematch(BLACK_IP, rule, "jo") then
-                    util.log_record('BlackList_IP', ngx.var_request_uri, "_", "_")
+                    util.log_record(config.config_log_dir,'BlackList_IP', ngx.var_request_uri, "_", "_")
                     if config.config_waf_enable == "on" then
                         ngx.exit(403)
                         return true
@@ -123,7 +123,7 @@ function _M.cc_attack_check()
         local req,_ = limit:get(CC_TOKEN)
         if req then
             if req > CCcount then
-                util.log_record('CC_Attack', ngx.var.request_uri, "-", "-")
+                util.log_record(config.config_log_dir,'CC_Attack', ngx.var.request_uri, "-", "-")
                 if config.config_waf_enable == "on" then
                     ngx.exit(403)
                 end
@@ -145,7 +145,7 @@ function _M.cookie_attack_check()
         if USER_COOKIE ~= nil then
             for _, rule in pairs(COOKIE_RULES) do
                 if rule ~="" and rulematch(USER_COOKIE, rule, "jo") then
-                    util.log_record('Deny_Cookie', ngx.var.request_uri, "-", rule)
+                    util.log_record(config.config_log_dir,'Deny_Cookie', ngx.var.request_uri, "-", rule)
                     if config.config_waf_enable == "on" then
                         util.waf_output()
                         return true
@@ -164,7 +164,7 @@ function _M.url_attack_check()
         local REQ_URI = ngx.var.request_uri
         for _,rule in pairs(URL_RULES) do
             if rule ~="" and rulematch(REQ_URI,rule,"jo") then
-                util.log_record('Deny_URL', REQ_URI, "-", rule)
+                util.log_record(config.config_log_dir,'Deny_URL', REQ_URI, "-", rule)
                 if config.config_waf_enable == "on" then
                     util.waf_output()
                     return true
@@ -191,7 +191,7 @@ function _M.url_args_attack_check()
                     ARGS_DATA = val
                 end
                 if ARGS_DATA and type(ARGS_DATA) ~= "boolean" and rule ~="" and rulematch(unescape(ARGS_DATA), rule, "jo") then
-                    util.log_record('Deny_URL_Args', ngx.var.request_uri, "-", rule)
+                    util.log_record(config.config_log_dir,'Deny_URL_Args', ngx.var.request_uri, "-", rule)
                     if config.config_waf_enable == "on" then
                         util.waf_output()
                         return true
@@ -238,7 +238,7 @@ function _M.post_attack_check()
                     post_data = v
                 end
                 if rule ~= "" and rulematch(post_data, rule, "jo") then
-                    util.log_record('Deny_USER_POST_DATA', post_data, "-", rule)
+                    util.log_record(config.config_log_dir,'Deny_USER_POST_DATA', post_data, "-", rule)
                     if config.config_waf_enable == "on" then
                         util.waf_output()
                         return true
